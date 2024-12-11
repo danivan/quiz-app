c0abf61ed223cd3cab45994c4609fdb7542222a1
Dan Sentillas <d.sentillas@arcanys.com>
2024-12-11 08:43:33 +0800

    create component for question card

---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{
+  item: string;
+}>();
+</script>
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div
+      class="flex flex-col rounded-xl bg-white p-4 shadow-xl w-1/3 h-1/2 self-center items-center justify-center"
+    >
+      <p class="text-2xl">{{ item.question }}</p>
+      <div>{{ item.choices }}</div>
+    </div>
+  </div>
+</template>
