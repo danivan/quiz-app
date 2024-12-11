2cb3531afd41f4c5ae274a1d666313747b426939
Dan Sentillas <d.sentillas@arcanys.com>
2024-12-11 09:14:44 +0800

    add button component for choices

---
+++ src/components/ChoiceButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="border-2 border-solid border-blue-500 hover:bg-blue-200 text-blue-500 font-bold py-2 px-4 rounded"
+  >
+    <slot></slot>
+  </button>
+</template>
