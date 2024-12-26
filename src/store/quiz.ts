03a570e5e973f9536095655b03162051cba566dc
Dan Sentillas <d.sentillas@arcanys.com>
2024-12-26 10:42:19 +0800

    add reactivity to card on wrong/right answers

--- src/store/quiz.ts
+++ src/store/quiz.ts
@@ -1,8 +1,35 @@
 import { defineStore } from 'pinia';
 import questions from '../../questions.json';
+import { ref } from 'vue';
 
 export const useQuizStore = defineStore('quiz', () => {
+  const currentQuestion = ref(questions[0]);
+  const score = ref(0);
+
+  function nextQuestion() {
+    if (currentQuestion.value.order === questions.length) {
+      return;
+    }
+    currentQuestion.value = questions[currentQuestion.value.order];
+  }
+
+  function incrementScore() {
+    score.value++;
+  }
+
+  function checkAnswer(choice: string): boolean {
+    if (choice === currentQuestion.value.answer) {
+      incrementScore();
+      return true;
+    }
+    return false;
+  }
+
   return {
     questions,
+    nextQuestion,
+    currentQuestion,
+    score,
+    checkAnswer,
   };
 });
